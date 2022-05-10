8fdf7cbb2cdde85e9431183de66c9e53592e0ac7
MuspiMerol <wjj123345567@iCloud.com>
2022-05-11 05:41:07 +0800

    all changes I forgot to upload

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from core.people import load, get_all_data
+from starlette.templating import Jinja2Templates
 
 app = FastAPI()
 
+try:
+    people = load(open("people.pkl", "rb"))
+except FileNotFoundError:
+    people = get_all_data()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+def to_json(name):
+    person = people[name]
+    result = {"简介": person.introduction, "相关人物": person.relevant_person}
+    if person.apartments:
+        result["相关机构"] = person.apartments
+    if person.subjects:
+        result["相关学科"] = person.subjects
+    result["相关事件"] = person.events
+    return result
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/person/{name}")
+def personal_page(request: Request, name: str):
+    return Jinja2Templates(".").TemplateResponse(
+        "template.html",
+        {
+            "request": request,
+            "title": name,
+            "person": to_json(name)
+        }
+    )
